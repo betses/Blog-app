b78a0907881fbee82d183b11323734e1f271a982
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-12-07 15:31:02 +0300

    Add devise and integrate users

--- db/migrate/20221124073632_create_users.rb
+++ db/migrate/20221124073632_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
-      t.text :bio
-      t.integer :posts_counter
+      t.string :photo, :default => 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png'
+      t.text :bio, :default => 'Put your bio here'
+      t.integer :posts_counter, :default => 0
 
       t.timestamps
     end
