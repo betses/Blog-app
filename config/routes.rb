9628fcc2f8370fc3d8dedca0678956cf9ca8be63
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-12-09 10:24:09 +0300

    Add api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # get '/user/:id', to: 'user#show', as: 'user'
   # get '/post/:user_id/posts', to: 'post#index', as: 'user_posts'
   # get '/post/:user_id/posts/:id', to: 'post#show', as: 'user_post'
+
   root 'users#index'
   resources :users, only: %i[show] do
     resources :posts, only: %i[index show new create destroy] do
@@ -15,4 +16,16 @@ Rails.application.routes.draw do
       resources :like, only: %i[create]
     end
   end
+# end
+
+
+namespace :api do
+  namespace :v1 do
+    post 'users/sign_in' => 'users#login'
+    get 'posts' => 'posts#index'
+    get 'comments' => 'comments#index'
+    post 'comments/create' => 'comments#create'
+  end
+end
+
 end
